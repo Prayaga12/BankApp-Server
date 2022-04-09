7c1b2c3ee7385efb102c6065047a56395cfb30cc
Prayaga <arprayaga12@gmail.com>
2022-04-09 11:45:08 +0530

    Complete Server without DataBase

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //Import Express
 const express = require('express')
+// const { verify } = require('jsonwebtoken')
 
 const dataService = require('./services/data.service')
+
+const jwt = require('jsonwebtoken')
 
 //Create an app using express
 const app = express()
@@ -37,22 +40,48 @@ app.delete('/',(req,res)=>{
     res.send("IT'S A DELETE METHOD")
 })
 
-//Bank App - API
+//Application Specific Middleware
+const appMiddleware = (req,res,next)=>{
+    console.log("Application Specific Middleware");
+    next()
+}
 
+app.use(appMiddleware)
+
+//To verify token - middleware
+const jwtMiddleware = (req,res,next)=>{
+    try{ 
+        // const token = req.body.token
+        const token = req.headers["x-access-token"]
+    //verify token
+        const data = jwt.verify(token,'supersecretkey123')
+        req.currentAcno = data.currentAcno
+        next()
+    }
+    catch{
+        res.status(422).json({
+            statusCode:422,
+            status:false,
+            message:"Please Log In"
+        })
+    }
+}
+
+
+
+//Bank App - API
 
 //Register API
 app.post('/register',(req,res)=>{ 
     const result = dataService.register(req.body.acno,req.body.password,req.body.uname)
     res.status(result.statusCode).json(result)
 })
-
     // if(result){
     //     res.send("Registered successfully")
     // }
     // else{
     //     res.send("Already exist!!! Please Log In...")
     // }
-
 
 //Login API
 app.post('/login',(req,res)=>{
@@ -62,9 +91,23 @@ app.post('/login',(req,res)=>{
 })
 
 //Deposit API
-app.post('/deposit',(req,res)=>{
+app.post('/deposit',jwtMiddleware,(req,res)=>{
     
     const result = dataService.deposit(req.body.acno,req.body.password,req.body.amt)
+    res.status(result.statusCode).json(result)
+})
+
+//Withdraw API
+app.post('/withdraw',jwtMiddleware,(req,res)=>{
+    
+    const result = dataService.withdraw(req,req.body.acno,req.body.password,req.body.amt)
+    res.status(result.statusCode).json(result)
+})
+
+//Transaction API
+app.post('/transaction',jwtMiddleware,(req,res)=>{
+    
+    const result = dataService.getTransaction(req.body.acno)
     res.status(result.statusCode).json(result)
 })
 
--- services/data.service.js
+++ services/data.service.js
@@ -1,3 +1,7 @@
+//Import jsonwebtoken
+const jwt = require('jsonwebtoken')
+
+
 database={
     1000:{acno:1000,uname:"Neema",password:1000,balance:5000,transaction:[]},
     1001:{acno:1001,uname:"Deepa",password:1001,balance:5000,transaction:[]},
@@ -39,12 +43,19 @@ const register = (acno,password,uname)=>{
             currentAcno = acno
             currentUname = database[acno]["uname"]
 
+            //Token Generation
+            const token = jwt.sign({
+              currentAcno:acno
+            },'supersecretkey123')
+
+
         return {
             statusCode:200,
             status:true,
             message:"Successfully Log in!!!",
             currentAcno,
-            currentUname
+            currentUname,
+            token
         }
       }
       else{
@@ -111,8 +122,98 @@ const deposit = (acno,password,amt)=>{
   }
 }
 
+//Withdraw definition
+
+const withdraw = (req,acno,password,amt)=>{
+
+  var amount = parseInt(amt)
+
+  var currentAcno = req.currentAcno
+
+  // let database = this.database
+
+  if(acno in database){
+
+    if(password == database[acno]["password"]){
+
+      if(currentAcno == acno){
+        if(database[acno]["balance"]>amount){
+          database[acno]["balance"]-=amount
+          database[acno]["transaction"].push({
+            amount:amount,                      //LHS-key RHS-value
+            type:"DEBIT"
+          })
+          console.log(database);
+          // this.storeData()
+          return {
+            statusCode:200,
+            status:true,
+            message:amount+" successfully debited...New balance is "+ database[acno]["balance"]
+          }
+        }
+        else{
+          // alert("Insufficient Balance")
+          return {
+            statusCode:422,
+            status:false,
+            message:"Insufficient Balance"
+          }
+        }
+
+      }
+      else{
+        return {
+          statusCode:422,
+          status:false,
+          message:"Operation denied!!!"
+        }
+      }
+
+      
+    }
+    else{
+      // alert("Incorrect password")
+      return {
+        statusCode:422,
+        status:false,
+        message:"Incorrect Password"
+      }
+    }
+  }
+  else{
+    // alert("User does not exist!!!")
+    return {
+      statusCode:422,
+      status:false,
+      message:"User does not exist!!!"
+    }
+  }
+}
+
+
+//Transaction definition
+const getTransaction = (acno)=>{
+
+  if(acno in database){
+    return {
+      statusCode:200,
+      status:true,
+      transaction:database[acno]["transaction"]
+    }
+  }
+  else{
+    return {
+      statusCode:422,
+      status:false,
+      message:"User does not exist!!!"
+    }
+  }
+}
+
   module.exports={
     register,
     login,
-    deposit
+    deposit,
+    withdraw,
+    getTransaction
   }
